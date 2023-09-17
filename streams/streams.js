0290a0c1c593c21d2e57858d30fca00c5797b943
brianruhiu <116198613+BrianRuhiuMuhia@users.noreply.github.com>
2023-09-17 00:06:19 -0700

    Update streams.js

--- streams/streams.js
+++ streams/streams.js
@@ -3,48 +3,48 @@
   const writeStream=createWriteStream("./write.txt")
   const {Duplex,PassThrough,Transform} =require("stream")
 
-//   readStream.on("data",(chunk)=>{
-// const result=writeStream.write(chunk)
-// if(!result)
-// {
-// console.log("backpressure")
-// readStream.pause()
-// }
-// })
-//   readStream.on("error",(err)=>{
-//     console.log(err)
-//   })
-// writeStream.on("drain",()=>
-// {
-//     console.log("drain")
-//     readStream.resume()
-// })
-//   readStream.on("end",()=>{
-// writeStream.end()
-//   })  
-// process.stdin.pipe(writeStream)
-// class Throttle extends Duplex
-// {
-//     constructor(ms)
-//     {
-// super()
-// this.delay=ms
-//     }
-//     _read()
-//     {
+   readStream.on("data",(chunk)=>{
+ const result=writeStream.write(chunk)
+ if(!result)
+ {
+ console.log("backpressure")
+ readStream.pause()
+ }
+ })
+   readStream.on("error",(err)=>{
+     console.log(err)
+   })
+ writeStream.on("drain",()=>
+ {
+     console.log("drain")
+     readStream.resume()
+ })
+   readStream.on("end",()=>{
+ writeStream.end()
+   })  
+ process.stdin.pipe(writeStream)
+ class Throttle extends Duplex
+ {
+     constructor(ms)
+     {
+ super()
+ this.delay=ms
+     }
+     _read()
+     {
 
-//      }
-//     _write(chunk,ecoding,callback)
-//     {
-//         this.push(chunk)
-//         setTimeout(callback,this.delay)
-//     }   
-//     _final(){
-//         this.push(null)
-//     }
-// }
-// const report=new PassThrough()
-// const throttle=new Throttle(100)
+      }
+     _write(chunk,ecoding,callback)
+     {
+         this.push(chunk)
+         setTimeout(callback,this.delay)
+     }   
+     _final(){
+         this.push(null)
+     }
+ }
+ const report=new PassThrough()
+ const throttle=new Throttle(100)
 class Replace extends Transform{
     constructor(text)
     {
